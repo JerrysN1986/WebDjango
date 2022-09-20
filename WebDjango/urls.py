97cb014a7bc409332daebb0c10b58463361cd8fa
JerrysN1986 <jerblockc1986@gmail.com>
2022-09-20 16:08:29 -0300

    inicializacion proyecto webDjango

--- WebDjango/urls.py
+++ WebDjango/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from WebDjango.view import home, homePage, cursos
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/<name>', home),
     path('homePage/', homePage),
-    path('cursos/', cursos)
+    path('cursos/', cursos),
+    path('AppCoder/', include("AppCoder.urls"))
+
 ]
